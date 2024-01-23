f1c81346fdce1ec0567667f462e5fdd2486ad71b
Darwin Manzano <manza_92@yahoo.com>
2024-01-23 17:24:14 -0600

    bases de js

--- 2034-logica-programacion-1-ProyectoInicial/app.js
+++ 2034-logica-programacion-1-ProyectoInicial/app.js
@@ -1,7 +1,8 @@
 let numeroSecreto = 6;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraVeces = 'vez';
+//let palabraVeces = 'vez';
+let maximoIntentos = 2;
 while (numeroUsuario != numeroSecreto) {
        numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor: ");
 
@@ -10,19 +11,21 @@ while (numeroUsuario != numeroSecreto) {
 
   if (numeroUsuario == numeroSecreto) {
     //Acertamos, fue verdadera la condición
-    alert(`Acertaste, el número es: ${numeroUsuario}. Lohiciste en ${intentos} ${palabraVeces}`);
-  } else {
-      if (numeroUsuario > numeroSecreto) {
-         alert("El numero secreto es menor");
-    } else {
-         alert("EL numero secreto es mayor");
-    }
-    //incrementar el contador cuando no acierta
-    intentos = intentos + 1;
-    palabraVeces = 'veces'
-    if (intentos > 3) {
-        alert('Llegaste al numero maximo de intentos');
-        break;
+     alert(`Acertaste, el número es: ${numeroUsuario}. Lohiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+  }  else {
+            if (numeroUsuario > numeroSecreto) {
+                alert("El numero secreto es menor");
+            } else {
+                alert("EL numero secreto es mayor");
+            }
+            //incrementar el contador cuando no acierta
+            // intentos = intentos + 1;
+            //intentos += 1;
+            intentos++;
+            //palabraVeces = 'veces'
+            if (intentos > maximoIntentos) {
+                alert(`Llegaste al numero maximo de ${maximoIntentos} intentos`);
+                break;
     }
   }
 }
